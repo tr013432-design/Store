4ebfe91149d43a8838b6bfbe5b1ca8bd9e79e920
tr013432-design <tr013432@gmail.com>
2026-01-30 11:21:24 -0300

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css' 
+// A linha do CSS foi removida daqui
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
